afe41fdf5e0e931f2d5e866172ffecca92866da3
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2015-12-22 22:39:39 -0800

    Readme + main methods

--- Readme.md
+++ Readme.md
@@ -1,9 +1,15 @@
-## Bookieman
+bookieman
+=============
+  
+<small>selwyn-lloyd mcpherson <<a href='mailto:selwyn.mcpherson@gmail.com'>selwynlloyd.mcpherson@gmail.com</a>></small>
 
-A tool for cross referencing best-selling lists and content trends.
+## Aim
+I was chatting with one of my very good friends, a post-doctoral student, who was seeking a book deal. He suggested that it might be possible to identify trends in certain topics and thus predict, in some sense, what will sell, and thus what a publisher would be willing to support.
 
-This is not viable code and is approximately 55% implemented.
+This is a very lofty goal and this solution certainly does not address all of the conditions that demand attention, but it is an interesting way to define trends in literature. 
 
-## Author
-Selwyn-Lloyd McPherson
-mypolopony@gmail.com
+## Implementation
+
+## Sources
+
+## Localization and Identification
--- bookieman.py
+++ bookieman.py
@@ -3,8 +3,8 @@
 # title          : bookieman.py
 # description    : Lets trend some books!
 # author         : Selwyn-Lloyd McPherson
-# date           : Fri Dec  4 02:00:54 PST 2015
-# version        : 1.20
+# date           : Fri Dec  11 02:00:54 PST 2015
+# version        : 1.3
 # python_version : 3.5.0 (it's the hot new thing)
 # ==========================================================
 
@@ -15,59 +15,39 @@
 
 
 from datetime import date, timedelta   # standard date module
+import pickle           # for binarizing results
+import ftfy             # great ascii / unicode fixer
+import redis            # redis is used to store our library of books
 import time             # standard time interface
 import requests         # standard web interface
-import json             # standard json interface
 import logging          # basic logging
 import credentials      # loads your API_KEY. This is for security,
                         # so you should create a file, credentials.py,
                         # with the line API_KEY = '###...'
-                        #
-                        #
-                        # or, for simplicity, you can just do:
-# API_KEY = '###...'    # (uncomment this line)
+                        
 
-# Setup and wayward home for global variables
+# Setup
 logging.basicConfig(filename='bookieman.log',level=logging.INFO)
+bookshelf = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Hello, I'm a book! 
 class Book():
-    def __init__(self,isbn,author,title,description,url):
+    def __init__(self,isbn,author,title,snippet,description,url):
         self.isbn = isbn
         self.title = title
         self.url = url
         self.author = author
         self.description = description
-
-
-# Hello, I'm a NYT list!
-class List():
-    def __init__(self,name):
-        self.dates = dict()
-
-    def add_instance(self,date,book,ranking):
-        if date not in self.instances.keys():
-            self.instances[date] = book
-        else:
-            self.instances[date].append(book)
-
-    def get_list(self,date):
-        return self['date']
-
-
-# Let's grab some books. At least, all the books that're 
-# fit to print.
-#
-# The New York Times API is freely available and the rate 
-# limits on the number of calls is reasonable if you're not 
-# up to something crazy.
-# 
-# developer.nytimes.com offers the free api key, so sign up!
+        self.snippet = snippet
+    
+    def serialize(self):
+        return {'isbn': self.isbn, 'title': self.title, 'url': self.url, 'author': self.author, 'description': self.description, 'snippet': self.snippet}
+    
 
 # Generate all days spaced seven apart from start to end 
 def get_weeks(start_date, end_date):
-    start_date = date(start_date[0],start_date[1],start_date[2])    # this is poor python
-    end_date = date(end_date[0],end_date[1],end_date[2])            # style :(
+    start_date = date(start_date[0],start_date[1],start_date[2]) 
+    end_date = date(end_date[0],end_date[1],end_date[2])
     
     weeks = list()
     for n in range(int ((end_date - start_date).days)):
@@ -75,35 +55,36 @@ def get_weeks(start_date, end_date):
 
     return weeks[0::7]              # return every seventh day
 
-def run():
-    # Grab the list of list names
+def get_list_names():
+    '''
+    Retrieves the various NYT Best Seller Lists
+    '''
     uri = 'http://api.nytimes.com/svc/books/v3/lists/names.json?api-key={key}'.format(key=credentials.NYT_API_KEY)
-    response = json.loads(requests.get(uri).text)
+    response = requests.get(uri).json()
     names = [r['list_name_encoded'] for r in response['results']]
     for idx,n in enumerate(names):
         logging.info('Available lists:')
-        logging.info('{i}\t{name}'.format(i=idx,name=names[idx]))
+        logging.info('{i}\t{name}'.format(i=idx,name=n))
+    return names
 
+def cleanse():
+    '''
+    Simple word cleaner
+    '''
+    
 
-    # Choose a list!
-    list_name = 'combined-print-fiction'
-    logging.info('Working with:\t{list}'.format(list=list_name))
-
-    # Pick a date range
-    start_date = [2011,1,1]                 # example for the year 2011
-    end_date = [2011,12,31]
-    weeks = get_weeks(start_date, end_date) # get evenly spaced weeks
-
-    # Set up the library of books, indexable by ISBN
-    bookshelf = dict()
+    
+def get_timeline(list_name,weeks):
+    # This timeline will hold the lists for a given week
     timeline = dict()
 
     for week in weeks:
         week = week.strftime('%Y-%m-%d')      # string formatting
         timeline[week] = list()
+        logging.info('Working on week: {w}'.format(w=week))
         
-        uri = 'http://api.nytimes.com/svc/books/v3/lists/{name}.json?date-{date}&api-key={key}'.format(date=week,name=list_name,key=credentials.NYT_API_KEY)
-        response = json.loads(requests.get(uri).text)
+        uri = 'http://api.nytimes.com/svc/books/v3/lists/{w}/{name}.json?api-key={k}'.format(name=list_name, w=week,k=credentials.NYT_API_KEY)
+        response = requests.get(uri).json()
         
         if len(response['results']):
             # Odd initialization, but I like it!
@@ -113,69 +94,120 @@ def run():
             for book in enumerate(response['results']['books']):
                 book = book[1]      # Response is a weird tuple with rank as the first element
                 
-                # Check ISBN first for efficiency
-                isbn = book['primary_isbn13']
-                logging.info('ISBN: {i}'.format(i=isbn))
-                
-                if isbn in bookshelf.keys():
-                    b = bookshelf[isbn]
-                    print('Found')
-                    
-                else:
-                    print('New')
-                    # This is the kind of serialization that is really gross, but easy to pull off
-                    rank = book['rank']
-                    logging.info('Ranking: {r}'.format(r=rank))
-    
-                    title = book['title']
-                    logging.info('Title: {t}'.format(t=title))
-                    
-                    author = book['author']
-                    logging.info('Author: {a}'.format(a=author))
-                    
-                    description = book['description'][:-1]  # weird marks at the end
-    
+                # One theme concerning ISBNs, or similarly UPCs, is that they
+                # are actually not entirely static, nor universal. ISBN-10 was
+                # phased out in 2007 in favor of ISBN=13. Unfortunately, both are
+                # used for various purposes. I'll use the new, preferred id
+                isbn10 = book['primary_isbn10']
+                isbn13 = book['primary_isbn13']                
+                rank = book['rank']
+                                
+                if not bookshelf.get(isbn13):
+                    title = ftfy.fix_text(book['title'])                  
+                    author = ftfy.fix_text(book['author'])                 
+                    snippet = ftfy.fix_text(book['description'][:-1])  # weird marks at the end
                     amazon_url = book['amazon_product_url']
-                    logging.info('Amazon URL: {a}'.format(a=amazon_url))
                     
-                    b = Book(isbn,author,title,description,uri)
-                    bookshelf[isbn] = b
+                    # The descriptions from the New York Times are abbreviated.
+                    # 
+                    # The Amazon product link can be used to grab a fuller 
+                    # description but that's messy. Amazon does have an API, 
+                    # but most people have found out the hard way that it is 
+                    # unnecessarily clunky.
+                    # 
+                    # The Google Books API works well [developers.google.com/books]
                     
+                    description = None
+                    uri = 'https://www.googleapis.com/books/v1/volumes?'
+                    # Try ISBN-13
+                    params = {'q':'isbn:{isbn}'.format(isbn=isbn13), 'key':credentials.GB_API_KEY}
+                    response = requests.get(uri,params=params)
+                    response = response.json()
+                    try:
+                        description = ftfy.fix_text(response['items'][0]['volumeInfo']['description'])
+                    except:
+                        pass
+                    # Try ISBN-10
+                    if not description:
+                        params = {'q':'isbn:{isbn}'.format(isbn=isbn10), 'key':credentials.GB_API_KEY}
+                        response = requests.get(uri,params=params)
+                        response = response.json()
+                        try:
+                            description = ftfy.fix_text(response['items'][0]['volumeInfo']['description'])
+                        except:
+                            pass
+                    # Try the title, as a last resort
+                    if not description:
+                        params = {'q':'title:{title}'.format(title=title), 'key':credentials.GB_API_KEY} 
+                        response = requests.get(uri,params=params)
+                        response = response.json()
+                        try:
+                            description = ftfy.fix_text(response['items'][0]['volumeInfo']['description'])
+                        except:
+                            logging.warning('Cannot find description for: {isbn}'.format(isbn=isbn10))
+                        
+                    b = Book(isbn13,author,title,snippet,description,amazon_url)
+                    bookshelf.set(isbn13,b.serialize())
+                    logging.debug(description)
+                    logging.info('New Book Added to Bookshelf: {t}'.format(t=b.title))
                 
+                # Depending on the time period, this should be fine in memory
                 # The -1 is necessary, because 1 != 0
-                timeline[week][rank-1] = b
-                
-                logging.info('-------')
-                print('-----------')
-                
-                # Depending on the time period, this should be fine in menory,
-                # but of course we can write to a file if necessary. . .
+                timeline[week][rank-1] = isbn13
                 
                 # Courtesy
                 time.sleep(.2)
-                    
-    # Now we have a dictionary, keyed by week, and books on the best seller list 
-    # on that week
-    #
-    # We were given the Amazon link
-    # 
-    # There are some different solutions here. The first is to use the
-    # requests module and manipulate the H/XTML response for each URI. That's a 
-    # bit messy, but certainly reliable in the short term.
-    #
-    # One might think, use the Amazon API! But most people have learned the 
-    # hard way that it is clunky and a bit too much.
-    #
-    # I prefer Google. Enable the Books API at [console.developers.google.com]
-    
-    
-    
-    
-    
-    
-    
 
-  
+# Let's grab some books. At least, all the books that're 
+# fit to print.
+#
+# The New York Times API is freely available and the rate 
+# limits on the number of calls is reasonable if you're not 
+# up to something crazy.
+# 
+# developer.nytimes.com offers the free api key, so sign up!   
+def run():
+    filename = 'paperback-nonfiction_[2011, 1, 1]_[2015, 11, 30]'
+    
+    # The New York Times actually has many lists for different genres
+    nyt_lists = get_list_names()
+    
+    # We'll choose just one
+    list_name = 'paperback-nonfiction'
+    logging.info('Working with:\t{list}'.format(list=list_name))
+    
+    # Pick a date range
+    start_date = [2011,1,1]                 # for example, 2011-2015
+    end_date = [2015,11,30]
+    weeks = get_weeks(start_date, end_date) # get evenly spaced weeks
+    
+    # Now to obtain the timeline
+    timeline = get_timeline(list_name,weeks)
+    pickle.dump(timeline,open('{ln}_{sd}_{ed}.dat'.format(ln=list_name,sd=start_date,ed=end_date),'wb'))
+
+    # Let's build a lexicon of words used in book descriptions. We'll save it
+    # since lexicons are always useful
+    lexicon = set()
+    isbns = bookshelf.keys('*')
+    for isbn in isbns:
+        description = eval(bookshelf.get(isbn))['description']
+        for word in description.split():
+            lexicon.add(cleanse(word.lower()))
+    logging.debug('Sorted lexicon:\n--------') # in debug, this can help know 
+    lexicon = set(sorted(lexicon))
+    # There is a tendency to use NLTK just for stop words. That's really not necessary 
+    with open('stopwords.txt','r') as sw:
+        stopwords = set(sw.read().splitlines())
+        lexicon = lexicon.difference(stopwords)
+    for word in lexicon:                       # what to filter out
+        logging.debug(word)
+    
+    
+        
+
+    
+                
+
 if __name__ == '__main__':
     run()
 
